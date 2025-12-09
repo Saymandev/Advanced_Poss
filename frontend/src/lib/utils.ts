f59335d9b3bc6b499b348ccf6287c60b0550beb8
Saymandev <devrabbi255@gmail.com>
2025-12-09 13:41:18 +0600

    build errors

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -71,7 +71,7 @@ export function formatDateTime(date: string | Date | null | undefined): string {
     minute: '2-digit',
   });
 }
-
+// new updated
 export function getInitials(firstName: string, lastName: string): string {
   return `${firstName.charAt(0)}${lastName.charAt(0)}`.toUpperCase();
 }
