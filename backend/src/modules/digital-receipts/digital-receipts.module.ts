ac8f0c9303cc63c0fc06de2f35167fd7c6491dec
Saymandev <devrabbi255@gmail.com>
2026-02-09 23:37:06 +0600

    changes

--- backend/src/modules/digital-receipts/digital-receipts.module.ts
+++ backend/src/modules/digital-receipts/digital-receipts.module.ts
@@ -1,10 +1,11 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { EmailService } from '../../common/services/email.service';
 import { CustomersModule } from '../customers/customers.module';
 import { POSModule } from '../pos/pos.module';
 import { POSOrder, POSOrderSchema } from '../pos/schemas/pos-order.schema';
-import { SubscriptionPlan, SubscriptionPlanSchema } from '../subscriptions/schemas/subscription-plan.schema';
+import { SubscriptionPlansModule } from '../subscriptions/subscription-plans.module';
+import { SubscriptionsModule } from '../subscriptions/subscriptions.module';
 import { DigitalReceiptsController } from './digital-receipts.controller';
 import { DigitalReceiptsService } from './digital-receipts.service';
 import { DigitalReceipt, DigitalReceiptSchema } from './schemas/digital-receipt.schema';
@@ -14,14 +15,15 @@ import { DigitalReceipt, DigitalReceiptSchema } from './schemas/digital-receipt.
     MongooseModule.forFeature([
       { name: DigitalReceipt.name, schema: DigitalReceiptSchema },
       { name: POSOrder.name, schema: POSOrderSchema },
-      { name: SubscriptionPlan.name, schema: SubscriptionPlanSchema },
     ]),
     POSModule,
     CustomersModule,
+    forwardRef(() => SubscriptionPlansModule),
+    forwardRef(() => SubscriptionsModule),
   ],
   controllers: [DigitalReceiptsController],
   providers: [DigitalReceiptsService, EmailService],
   exports: [DigitalReceiptsService],
 })
-export class DigitalReceiptsModule {}
+export class DigitalReceiptsModule { }
 
